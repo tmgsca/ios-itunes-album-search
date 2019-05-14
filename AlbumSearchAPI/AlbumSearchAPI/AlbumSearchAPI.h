73db0ae31f4b3afb83c8e284a3bb2013e1b94fbf
Thiago Magalhaes <tmgsca@gmail.com>
2019-05-14 19:12:38 -0400

    Adding Main and API projects

---
+++ AlbumSearchAPI/AlbumSearchAPI/AlbumSearchAPI.h
@@ -0,0 +1,19 @@
+//
+//  AlbumSearchAPI.h
+//  AlbumSearchAPI
+//
+//  Created by Thiago Magalhaes on 2019-05-14.
+//  Copyright © 2019 Thiago Magalhaes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlbumSearchAPI.
+FOUNDATION_EXPORT double AlbumSearchAPIVersionNumber;
+
+//! Project version string for AlbumSearchAPI.
+FOUNDATION_EXPORT const unsigned char AlbumSearchAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlbumSearchAPI/PublicHeader.h>
+
+
